4b77bd127b4a40727e4bf8f7b7226fba9744ef05
C32-2 <vasilybichutsky@gmail.com>
2026-03-15 13:58:10 +0300

    Initialized backend boostrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
-val exposed_version: String by project
-val h2_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     kotlin("jvm") version "2.3.0"
     id("io.ktor.plugin") version "3.4.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
     id("org.flywaydb.flyway") version "11.14.1"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.flywaydb:flyway-database-postgresql:11.14.1")
+        classpath("org.postgresql:postgresql:42.7.7")
+    }
 }
 
 group = "com.vb"
@@ -21,22 +26,31 @@ kotlin {
     jvmToolchain(24)
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-call-logging")
     implementation("io.ktor:ktor-server-host-common")
     implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-request-validation")
     implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("org.postgresql:postgresql:42.7.7")
     implementation("org.flywaydb:flyway-core:11.14.1")
     implementation("org.flywaydb:flyway-database-postgresql:11.14.1")
+
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.3.0")
+}
+
+flyway {
+    url = providers.gradleProperty("flywayUrl").get()
+    user = providers.gradleProperty("flywayUser").get()
+    password = providers.gradleProperty("flywayPassword").get()
 }
