1dfdb5c9b32d517ea1256d1902a7c9a83641b678
C32-2 <vasilybichutsky@gmail.com>
2026-03-15 14:52:59 +0300

    Added correct schema

--- src/main/resources/db/migration/V1__create_map_table.sql
+++ src/main/resources/db/migration/V1__create_map_table.sql
@@ -1,3 +1,11 @@
-CREATE TABLE IF NOT EXISTS map (
-    id INT PRIMARY KEY
-)
+CREATE TABLE IF NOT EXISTS map(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    slug TEXT NOT NULL UNIQUE,
+    description TEXT,
+    pmtiles_path TEXT NOT NULL,
+    preview_path TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_map_created_at ON map(created_at);
